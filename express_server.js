efd477c115e8ee44d5867317d5c7f1ab30b26e57
Cecilia <ysycecilia@gmail.com>
2021-06-08 02:52:31 +0000

    create a redirect to longURL

--- express_server.js
+++ express_server.js
@@ -63,6 +63,11 @@ app.post("/urls", (req, res) => {
   res.send("ok");
 });
 
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
