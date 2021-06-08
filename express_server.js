b6e785163a949659a0b6f2a133ae4106cae571eb
Cecilia <ysycecilia@gmail.com>
2021-06-08 00:15:47 +0000

    create view for /urls by using ejs

--- express_server.js
+++ express_server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
-const PORT = 8080;
+const PORT = 8090;
 
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -17,7 +18,9 @@ app.get("/urls.json", (req, res) => {
 });
 
 app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
+  const templateVars = { greeting: 'Hello World!' };
+  res.render("hello_world", templateVars);
+
 });
 
 app.get("/set", (req, res) => {
@@ -25,9 +28,10 @@ app.get("/set", (req, res) => {
   res.send(`a = ${a}`);
 });
 
-// app.get("/fetch", (req, res) => {
-//   res.send(`a = ${a}`);
-// });
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
