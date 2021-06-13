537eb89cf00d01a514f8159933ee2b596a2f5414
Cecilia <ysycecilia@gmail.com>
2021-06-13 01:54:44 +0000

    cleanup

--- express_server.js
+++ express_server.js
@@ -3,7 +3,7 @@ const cookieSession = require('cookie-session');
 const bcrypt = require("bcrypt");
 const { generateRandomString, urlsForUser, getUserByEmail, getEmailBySessionID, getShortURLByUserID} = require('./helpers');
 
-const PORT = 8090;
+const PORT = 8080;
 
 const app = express();
 
